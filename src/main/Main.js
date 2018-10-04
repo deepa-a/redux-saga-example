625b971a374987763f7a2bada6819bd273526092
Arnon Ruangthanawes <arnon.raungthanawes@unico.com.au>
2018-10-04 16:18:09 +1000

    DSIM-301 Create application routes - remove space

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import mainStyle from 'assets/jss/main/mainStyle';
 import { withStyles } from '@material-ui/core';
 import { getSwitchRoute } from '../route/RouteUtil';
