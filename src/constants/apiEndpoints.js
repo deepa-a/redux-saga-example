96e4513e13578406453d55daff2d9d9d8fbb3d23
deepa-a <Deepa.Aravindan@unico.com.au>
2018-10-10 15:04:10 +1100

    trying an API call with axios. we still need to solve the cors issue

---
+++ src/constants/apiEndpoints.js
@@ -0,0 +1,11 @@
+export const API_BASE_URL = process.env.API_BASE_URL + 'ocsia-selfcare/prepaid/rest/v1/';
+
+
+export const ENDPOINTS = {
+    SUBSCRIBER: {
+        GET: {
+            URL: 'subscribers'
+        }
+    }
+};
+
