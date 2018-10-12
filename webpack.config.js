4a2f257bf7e99ae7fc7145c5f090cfc6ebf43258
UNICO-AUS\lli <leon.li@unico.com.au>
2018-10-12 18:06:00 +1100

    Corrected all linting warnings in webpakc.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,96 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const webpack = require("webpack");
-const env_var = require('./environment.config.js');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 const path = require('path');
+const envVar = require('./environment.config.js');
 
-module.exports = (env, argv) => {
-    var API_BASE_URL;
-    switch (env.NODE_ENV) {
-        case 'development':
-            API_BASE_URL = env_var.DEVELOPMENT_API_URL;
-            break;
-        case 'production':
-            API_BASE_URL = env_var.DEMO1_API_URL;
-            break;
-        default:
-            API_BASE_URL = env_var.DEMO1_API_URL;
-    }
-    return {
-        entry: ["@babel/polyfill", "./src/index.js"],
-        context: __dirname,
-        resolve: {
-            modules: [path.resolve(__dirname, './src'), 'node_modules'],
-            extensions: ['.js', '.jsx', '.json']
+module.exports = (env) => {
+  let API_BASE_URL;
+  switch (env.NODE_ENV) {
+    case 'development':
+      API_BASE_URL = envVar.DEVELOPMENT_API_URL;
+      break;
+    case 'production':
+      API_BASE_URL = envVar.DEMO1_API_URL;
+      break;
+    default:
+      API_BASE_URL = envVar.DEMO1_API_URL;
+  }
+  return {
+    entry: ['@babel/polyfill', './src/index.js'],
+    context: __dirname,
+    resolve: {
+      modules: [path.resolve(__dirname, './src'), 'node_modules'],
+      extensions: ['.js', '.jsx', '.json'],
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        output: {
-            filename: '[name].[contenthash].js',
-            publicPath: '/'
+      }, {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: true,
+          },
+        }],
+      }, {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }, {
+        test: /\.(png|jpeg|gif)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '/assets/images/[name].[ext]',
+          },
+        }],
+      }],
+    },
+    devServer: {
+      compress: true,
+      historyApiFallback: true,
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+        favicon: './public/favicon.ico',
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[id].[contenthash].css',
+      }),
+      new CopyWebpackPlugin([{
+        from: 'public/manifest.json',
+      }]),
+      new webpack.DefinePlugin({
+        'process.env': {
+          API_BASE_URL: JSON.stringify(API_BASE_URL),
         },
-        module: {
-            rules: [{
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader"
-                    }
-                },
-                {
-                    test: /\.html$/,
-                    use: [{
-                        loader: "html-loader",
-                        options: {
-                            minimize: true
-                        }
-                    }]
-                },
-                {
-                    test: /\.css$/,
-                    use: [MiniCssExtractPlugin.loader, "css-loader"]
-                },
-                {
-                    test: /\.(png|jpeg|gif)$/,
-                    use: [{
-                        loader: "file-loader",
-                        options: {
-                            name: '/assets/images/[name].[ext]'
-                        }
-                    }]
-                }
-            ]
+      }),
+    ],
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
         },
-        devServer: {
-            compress: true,
-            historyApiFallback: true
-        },
-        plugins: [
-            new HtmlWebPackPlugin({
-                template: "./public/index.html",
-                filename: "./index.html",
-                favicon: "./public/favicon.ico"
-            }),
-            new MiniCssExtractPlugin({
-                filename: "[name].[contenthash].css",
-                chunkFilename: "[id].[contenthash].css"
-            }),
-            new CopyWebpackPlugin([{
-                from: 'public/manifest.json'
-            }]),
-            new webpack.DefinePlugin({
-                'process.env': {
-                    API_BASE_URL: JSON.stringify(API_BASE_URL)
-                }
-            })
-        ],
-        optimization: {
-            runtimeChunk: 'single',
-            splitChunks: {
-                chunks: 'all',
-                cacheGroups: {
-                    vendor: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: 'vendors',
-                        chunks: 'all'
-                    }
-                }
-            }
-        }
-    }
+      },
+    },
+  };
 };
