e38871da6a27f5df024fea491fdade6d1227c5c4
UNICO-AUS\lli <leon.li@unico.com.au>
2018-10-30 14:28:10 +1100

    Added clicking mechanism for part of the menu. Removed redux-thunk.

--- src/getStore.js
+++ src/getStore.js
@@ -1,6 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
-import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import reducer from './reducers/reducers';
 import rootSaga from './sagas';
@@ -14,9 +13,9 @@ export default function configureStore(initialState) {
 
   const getMiddleware = () => {
     if (process.env.NODE_ENV === 'development') {
-      return applyMiddleware(sagaMiddleware, thunk, logger);
+      return applyMiddleware(sagaMiddleware, logger);
     }
-    return applyMiddleware(sagaMiddleware, thunk);
+    return applyMiddleware(sagaMiddleware);
   };
 
   const store = createStore(
